7ed2901ab39aa05dffb86495242a12e4d453f4b1
Malinda Ilankoon <malindailankoon2003@gmail.com>
2026-01-23 14:54:34 +0530

    make the line buffer

--- rtl/line_buffer.sv
+++ rtl/line_buffer.sv
@@ -1,82 +1,73 @@
-// `timescale 1ns / 100ps
-
-// module line_buffer(
-//     input logic clk, rst,
-//     input logic [7:0] data_in,
-//     input logic data_valid,
-//     output logic [23:0] data_out, // outputs 3 pixels needed for a 3x3 pixel convolution. note that verilog doesn't allow multidimensional port declarations
-//     input logic rd_data
-// );
-
-//     logic [7:0] line [511:0]; // line buffer
-//     logic [8:0] wrtptr, rdptr;
-
-//     // modelling how data is written
-//     always_ff @( posedge clk ) begin 
-//         if (data_valid)
-//             line[wrtptr] <= data_in;
-//     end
-
-//     // updating the wrtptr
-//     always_ff @(posedge clk) begin
-//         if (rst) 
-//             wrtptr <= 'd0;
-//         else if (data_valid)
-//             wrtptr <= wrtptr + 'd1;
-//     end
-
-//     assign data_out = {line[rdptr], line[rdptr+1], line[rdptr+2]};
-
-//     always_ff @(posedge clk) begin
-//         if (rst)
-//             rdptr <= 'd0;
-//         else if (rd_data)
-//             rdptr <= rdptr + 'd1;
-//     end
-    
-// endmodule
-
-
-
-
-`timescale 1ns / 100ps
-
-module lineBuffer(
-input   i_clk,
-input   i_rst,
-input [7:0] i_data,
-input   i_data_valid,
-output [23:0] o_data,
-input i_rd_data
+module line_buffer (
+    input logic         clk, rst,
+    input logic [7:0]   data_in,
+    input logic         data_in_valid,
+    input logic         rd_req,
+    output logic [23:0] data_out,
+    output logic        full,
+    output logic        done
 );
 
-reg [7:0] line [511:0]; //line buffer
-reg [8:0] wrPntr;
-reg [8:0] rdPntr;
+    logic [7:0] line [0:511];
+    logic [8:0] wrtptr, rdptr;
+    logic [7:0] d0, d1, d2;
+    logic shift_en;
 
-always @(posedge i_clk)
-begin
-    if(i_data_valid)
-        line[wrPntr] <= i_data;
-end
+    assign full = &wrtptr;
+    assign done = &rdptr;
 
-always @(posedge i_clk)
-begin
-    if(i_rst)
-        wrPntr <= 'd0;
-    else if(i_data_valid)
-        wrPntr <= wrPntr + 'd1;
-end
-
-assign o_data = {line[rdPntr],line[rdPntr+1],line[rdPntr+2]};
-
-always @(posedge i_clk)
-begin
-    if(i_rst)
-        rdPntr <= 'd0;
-    else if(i_rd_data)
-        rdPntr <= rdPntr + 'd1;
-end
+    // advancing of the wrtptr
+    always_ff @( posedge clk ) begin
+        if (rst)
+            wrtptr <= '0;
+        else if (data_in_valid) 
+            wrtptr <= wrtptr + 1;
+    end
 
 
+    // writing logic to the buffer
+    always_ff @(posedge clk) begin
+        if (data_in_valid)
+            line[wrtptr] <= data_in;
+    end
+
+    always_comb begin
+        shift_en = 1'b0;
+        if (wrtptr == 9'd1 || wrtptr == 9'd2 || wrtptr == 9'd3 || rd_req) begin
+            shift_en = 1'b1;
+        end
+    end
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            rdptr <= 0;
+        end
+        else begin
+            if (((wrtptr == 9'd1 || wrtptr == 9'd2 || wrtptr == 9'd3) && data_in_valid) || rd_req) begin
+                rdptr <= rdptr + 1;
+            end
+            else begin
+                rdptr <= rdptr;
+            end
+        end
+    end
+
+
+    // shift register logic 
+    always_ff @(posedge clk) begin
+        if (shift_en) begin
+            d0 <= line[rdptr];
+            d1 <= d0;
+            d2 <= d1;
+        end
+    end
+
+
+    // data out
+    assign data_out = {d0, d1, d2};
+    
+
+
+
+    
 endmodule
