ffaa1b42d23e5e306fb96fd855c20e14889cffe2
Malinda Ilankoon <163558509+malindailankoon@users.noreply.github.com>
2026-03-01 01:11:05 +0530

    Delete test_accum_stage/sim/sim_build directory

--- test_accum_stage/sim/sim_build/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
-  /Users/malindailankoon/Programming/verilator-5.045/include/verilated_threads.cpp \
-  /Users/malindailankoon/Programming/verilator-5.045/include/verilatedos.h \
-  /Users/malindailankoon/Programming/verilator-5.045/include/verilated_threads.h \
-  /Users/malindailankoon/Programming/verilator-5.045/include/verilated.h \
-  /Users/malindailankoon/Programming/verilator-5.045/include/verilated_config.h \
-  /Users/malindailankoon/Programming/verilator-5.045/include/verilated_types.h \
-  /Users/malindailankoon/Programming/verilator-5.045/include/verilated_funcs.h
